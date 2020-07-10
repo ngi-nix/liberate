c4f42381753b8825fa6660642b8e1962088b41e1
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-07-10 11:51:10 +0200

    Add URL class

---
+++ include/liberate/net/url.h
@@ -0,0 +1,120 @@
+/**
+ * This file is part of liberate.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2018-2020 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef LIBERATE_NET_URL_H
+#define LIBERATE_NET_URL_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <liberate.h>
+
+#include <string>
+#include <map>
+
+#include <liberate/cpp/operators.h>
+
+namespace liberate::net {
+
+/**
+ * The URL class provides *minimal* URL parsing for specifying connector.
+ *
+ * That is, more advanced features of proper URLs are ignored - such as
+ * using URL escaping, or quoting, etc. The main goal is to split a URL
+ * into:
+ * - scheme
+ * - authority (host)
+ * - path
+ * - query parameters
+ * - fragment
+ *
+ * The logic is simple enough that, for example, the first # found is
+ * assumed to be the fragment delimiter, etc. It also does not deal with
+ * duplicate parameters; the last found always wins.
+ *
+ * One thing that it does, however, is lower-case the scheme, all query
+ * parameters and their values, and convert all values indicating
+ * boolean values to simple "0" and "1" (e.g. "true", "yes", etc.).
+ */
+struct LIBERATE_API url
+  : public ::liberate::cpp::operators<url>
+{
+public:
+  // Data members
+  std::string                         scheme;
+  std::string                         authority;
+  std::string                         path;
+  std::map<std::string, std::string>  query;
+  std::string                         fragment;
+
+  // Static parse functions
+  static url parse(char const * url_string);
+  static url parse(std::string const & url_string);
+
+  // Convert back to string
+  std::string str() const;
+
+  // Calculate hash/make Swappable
+  void swap(url & other);
+  size_t hash() const;
+
+private:
+  friend struct liberate::cpp::operators<url>;
+
+  bool is_less_than(url const & other) const;
+  bool is_equal_to(url const & other) const;
+
+  friend LIBERATE_API_FRIEND std::ostream & operator<<(std::ostream & os, url const & data);
+};
+
+
+/**
+ * Formats a url into human-readable form.
+ **/
+LIBERATE_API std::ostream & operator<<(std::ostream & os, url const & data);
+
+/**
+ * Swappable
+ **/
+inline void
+swap(url & first, url & second)
+{
+  return first.swap(second);
+}
+
+} // namespace liberate::net
+
+/*******************************************************************************
+ * std namespace specializations
+ **/
+namespace std {
+
+template <> struct LIBERATE_API hash<::liberate::net::url>
+{
+  size_t operator()(::liberate::net::url const & x) const
+  {
+    return x.hash();
+  }
+};
+
+} // namespace std
+
+
+#endif // guard
---
+++ test/net/url.cpp
@@ -0,0 +1,258 @@
+/**
+ * This file is part of liberate.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2018-2020 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+
+#include <liberate/net/url.h>
+
+#include <gtest/gtest.h>
+
+namespace net = liberate::net;
+
+TEST(URL, complete)
+{
+  // With everything
+  auto url = net::url::parse("https://finkhaeuser.de/path/to?some=value&simple&other=tRue#myfrag");
+  ASSERT_EQ(std::string("https"), url.scheme);
+  ASSERT_EQ(std::string("finkhaeuser.de"), url.authority);
+  ASSERT_EQ(std::string("/path/to"), url.path);
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+}
+
+
+TEST(URL, authority_path_and_fragment)
+{
+  // With fragment, no query
+  auto url = net::url::parse("https://finkhaeuser.de/path/to/#myfrag");
+  ASSERT_EQ(std::string("https"), url.scheme);
+  ASSERT_EQ(std::string("finkhaeuser.de"), url.authority);
+  ASSERT_EQ(std::string("/path/to/"), url.path);
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+
+  // With query, no fragment
+  url = net::url::parse("https://finkhaeuser.de/path/to?some=value&simple&other=tRue");
+  ASSERT_EQ(std::string("https"), url.scheme);
+  ASSERT_EQ(std::string("finkhaeuser.de"), url.authority);
+  ASSERT_EQ(std::string("/path/to"), url.path);
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, authority_path_and_query)
+{
+  // With query, no fragment
+  auto url = net::url::parse("https://finkhaeuser.de/path/to?some=value&simple&other=tRue");
+  ASSERT_EQ(std::string("https"), url.scheme);
+  ASSERT_EQ(std::string("finkhaeuser.de"), url.authority);
+  ASSERT_EQ(std::string("/path/to"), url.path);
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, authority_query_and_fragment)
+{
+  // Query and fragment, but no path
+  auto url = net::url::parse("https://finkhaeuser.de?some=value&simple&other=tRue#myfrag");
+  ASSERT_EQ(std::string("https"), url.scheme);
+  ASSERT_EQ(std::string("finkhaeuser.de"), url.authority);
+  ASSERT_EQ(0, url.path.length());
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+}
+
+TEST(URL, path_and_fragment)
+{
+  // No authority, but path and framgnent
+  auto url = net::url::parse("file:///path/to?some=value&simple&other=tRue#myfrag");
+  ASSERT_EQ(std::string("file"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(std::string("/path/to"), url.path);
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+
+}
+
+TEST(URL, path_and_query)
+{
+  // No authority, but path and query. No fragment.
+  auto url = net::url::parse("file:///path/to#myfrag");
+  ASSERT_EQ(std::string("file"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(std::string("/path/to"), url.path);
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+}
+
+TEST(URL, query_and_fragment)
+{
+  // No authority, no path, just query and fragment.
+  auto url = net::url::parse("file://?some=value&simple&other=tRue#myfrag");
+  ASSERT_EQ(std::string("file"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(0, url.path.length());
+  ASSERT_EQ(3, url.query.size());
+
+  auto iter = url.query.find("some");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("value"), iter->second);
+
+  iter = url.query.find("simple");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  iter = url.query.find("other");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("1"), iter->second);
+
+  ASSERT_EQ(std::string("myfrag"), url.fragment);
+}
+
+
+TEST(URL, IP_address)
+{
+  auto url = net::url::parse("TcP4://127.0.0.1:123");
+  ASSERT_EQ(std::string("tcp4"), url.scheme);
+  ASSERT_EQ(std::string("127.0.0.1:123"), url.authority);
+  ASSERT_EQ(0, url.path.length());
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, anon)
+{
+  auto url = net::url::parse("anon://");
+  ASSERT_EQ(std::string("anon"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(0, url.path.length());
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, local)
+{
+  auto url = net::url::parse("local:///foo/bar");
+  ASSERT_EQ(std::string("local"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(std::string("/foo/bar"), url.path);
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, pipe)
+{
+  auto url = net::url::parse("pipe:///foo/bar");
+  ASSERT_EQ(std::string("pipe"), url.scheme);
+  ASSERT_EQ(0, url.authority.length());
+  ASSERT_EQ(std::string("/foo/bar"), url.path);
+  ASSERT_EQ(0, url.query.size());
+  ASSERT_EQ(0, url.fragment.length());
+}
+
+
+TEST(URL, non_blocking)
+{
+  auto url = net::url::parse("pipe:///foo/bar?blocking=false");
+  ASSERT_EQ(1, url.query.size());
+
+  auto iter = url.query.find("blocking");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("0"), iter->second);
+}
+
+
+TEST(URL, datagram)
+{
+  auto url = net::url::parse("pipe:///foo/bar?behaviour=datagram");
+  ASSERT_EQ(1, url.query.size());
+
+  auto iter = url.query.find("behaviour");
+  ASSERT_NE(url.query.end(), iter);
+  ASSERT_EQ(std::string("datagram"), iter->second);
+}
