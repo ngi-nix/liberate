4e57358f4f0aaf49a1af47f0d4941040c285b5b9
Jens Finkhaeuser <jens@finkhaeuser.de>
2020-07-17 14:15:46 +0200

    enable_if compatible type traits for detecting bit size of values

---
+++ include/liberate/types/type_traits.h
@@ -0,0 +1,92 @@
+/**
+ * This file is part of liberate.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2020 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#ifndef LIBERATE_TYPES_TYPE_TRAITS_H
+#define LIBERATE_TYPES_TYPE_TRAITS_H
+
+#ifndef __cplusplus
+#error You are trying to include a C++ only header file
+#endif
+
+#include <liberate.h>
+
+#include <type_traits>
+
+namespace liberate::types {
+
+/**
+ * An integral constant that is either equivalent to std::true_type or
+ * std::false_type, depending on whether the byte size of the given type
+ * is the given value.
+ */
+template <typename T, std::size_t BYTE_SIZE>
+using has_byte_size = std::integral_constant<
+  bool,
+  sizeof(T) == BYTE_SIZE
+>;
+
+
+/**
+ * Determine whether the type has 8, 16, 32, 64 or 128 bits in
+ * size. There is not much use for other sizes at the moment.
+ */
+template <typename T>
+struct is_8bit_type
+  : public has_byte_size<
+      typename std::remove_cv<T>::type,
+      1
+    >::type
+{};
+
+template <typename T>
+struct is_16bit_type
+  : public has_byte_size<
+      typename std::remove_cv<T>::type,
+      2
+    >::type
+{};
+
+template <typename T>
+struct is_32bit_type
+  : public has_byte_size<
+      typename std::remove_cv<T>::type,
+      4
+    >::type
+{};
+
+template <typename T>
+struct is_64bit_type
+  : public has_byte_size<
+      typename std::remove_cv<T>::type,
+      8
+    >::type
+{};
+
+template <typename T>
+struct is_128bit_type
+  : public has_byte_size<
+      typename std::remove_cv<T>::type,
+      16
+    >::type
+{};
+
+
+} // namespace liberate::types
+
+#endif // guard
---
+++ test/types/type_traits.cpp
@@ -0,0 +1,97 @@
+/**
+ * This file is part of liberate.
+ *
+ * Author(s): Jens Finkhaeuser <jens@finkhaeuser.de>
+ *
+ * Copyright (c) 2020 Jens Finkhaeuser.
+ *
+ * This software is licensed under the terms of the GNU GPLv3 for personal,
+ * educational and non-profit use. For all other uses, alternative license
+ * options are available. Please contact the copyright holder for additional
+ * information, stating your intended usage.
+ *
+ * You can find the full text of the GPLv3 in the COPYING file in this code
+ * distribution.
+ *
+ * This software is distributed on an "AS IS" BASIS, WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ **/
+#include <liberate/types/type_traits.h>
+
+#include <gtest/gtest.h>
+
+#include <cstddef>
+
+TEST(TypesTraits, has_byte_size)
+{
+  using namespace liberate::types;
+
+  // Spot checks.
+
+  // 8-bit must be 1 byte and must not be 2 byte
+  ASSERT_TRUE((has_byte_size<uint8_t, 1>::value));
+  ASSERT_FALSE((has_byte_size<uint8_t, 2>::value));
+
+  // 16-bit must be 2 byte and must not be 3 byte
+  ASSERT_TRUE((has_byte_size<uint16_t, 2>::value));
+  ASSERT_FALSE((has_byte_size<uint16_t, 3>::value));
+
+  // 64 bit signed and unsigned must both be 8 byte
+  ASSERT_TRUE((has_byte_size<uint64_t, 8>::value));
+  ASSERT_TRUE((has_byte_size<int64_t, 8>::value));
+}
+
+
+TEST(TypesTraits, is_8bit_type)
+{
+  using namespace liberate::types;
+
+  // True
+  ASSERT_TRUE(is_8bit_type<uint8_t>::value);
+  ASSERT_TRUE(is_8bit_type<int8_t>::value);
+  ASSERT_TRUE(is_8bit_type<char>::value);
+  ASSERT_TRUE(is_8bit_type<signed char>::value);
+  ASSERT_TRUE(is_8bit_type<unsigned char>::value);
+  ASSERT_TRUE(is_8bit_type<std::byte>::value);
+
+  ASSERT_FALSE(is_8bit_type<uint16_t>::value);
+}
+
+
+TEST(TypesTraits, is_16bit_type)
+{
+  using namespace liberate::types;
+
+  // True
+  ASSERT_TRUE(is_16bit_type<uint16_t>::value);
+  ASSERT_TRUE(is_16bit_type<int16_t>::value);
+  ASSERT_TRUE(is_16bit_type<char16_t>::value);
+
+  ASSERT_FALSE(is_16bit_type<uint8_t>::value);
+}
+
+
+TEST(TypesTraits, is_32bit_type)
+{
+  using namespace liberate::types;
+
+  // True
+  ASSERT_TRUE(is_32bit_type<uint32_t>::value);
+  ASSERT_TRUE(is_32bit_type<int32_t>::value);
+  ASSERT_TRUE(is_32bit_type<char32_t>::value);
+
+  ASSERT_FALSE(is_32bit_type<uint8_t>::value);
+}
+
+
+TEST(TypesTraits, is_64bit_type)
+{
+  using namespace liberate::types;
+
+  // True
+  ASSERT_TRUE(is_64bit_type<uint64_t>::value);
+  ASSERT_TRUE(is_64bit_type<int64_t>::value);
+
+  ASSERT_FALSE(is_64bit_type<uint8_t>::value);
+}
